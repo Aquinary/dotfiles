d39d88b3acb397f57171edb0d2f725ddedd92a89
aquinary <git@aquinary.ru>
2023-03-20 15:25:30 +0300

    Linked git conf to /etc/nixos conf

---
+++ linked.sh
@@ -0,0 +1,5 @@
+rm /etc/nixos/configuration.nix
+rm /etc/nixos/hardware-configuration.nix
+
+ln -s /home/aquinary/git/nixos/configuration.nix /etc/nixos/configuration.nix
+ln -s /home/aquinary/git/nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
